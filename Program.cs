5abb8930536ee32e829b30844a5d291481f4a92e
GabeDebug <gabrielmessiasc0d3@gmail.com>
2026-04-20 11:40:55 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using Blog.Data;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog
+{
+    class Program
+    {
+        static void Main(string[] args) {}
+    }
+}
